84aa1eb1902942b6bdfba9353bb63b833bfc944d
김희정 <heejung030619@gmail.com>
2025-10-28 16:54:20 +0900

    오류 못찾음
    하...

--- Engine.cpp
+++ Engine.cpp
@@ -1,10 +1,9 @@
-#include<iostream>
-#include<fstream>
-#include<string>
-#include<conio.h>
-#include<algorithm>
-
 #include "Engine.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <conio.h>
+#include <algorithm>
 
 #include "World.h"
 #include "Wall.h"
@@ -16,13 +15,13 @@
 #include "Timer.h"
 #include "Input.h"
 
-//using namespace std;
 
 //FEngine* GEngine = nullptr;
 
 FEngine* FEngine::Instance = nullptr;
 
-FEngine::FEngine() : World(nullptr), MyEvent(SDL_Event())
+FEngine::FEngine() :
+	World(nullptr), MyEvent(SDL_Event())
 {
 	MyRenderer = nullptr;
 	MyWindow = nullptr;
@@ -36,6 +35,7 @@ FEngine::~FEngine()
 	{
 		delete World;
 	}
+
 	if (Timer)
 	{
 		delete Timer;
@@ -51,11 +51,10 @@ void FEngine::Init()
 {
 	SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO);
 
-	MyWindow = SDL_CreateWindow("GameEngine", 640, 480, SDL_WINDOW_OPENGL);
+	MyWindow = SDL_CreateWindow("Engine", 800, 600, SDL_WINDOW_OPENGL);
 	MyRenderer = SDL_CreateRenderer(MyWindow, nullptr);
 
 	OpenLevel();
-	
 }
 
 void FEngine::OpenLevel()
@@ -64,73 +63,64 @@ void FEngine::OpenLevel()
 
 	World = new UWorld();
 
-	std::ifstream Map("level01.map");
+	std::ifstream MapFile("level01.map");
 
-	if (Map.is_open())
+	if (MapFile.is_open())
 	{
 		char Buffer[1024] = { 0, };
 		int Y = 0;
-
-		while (Map.getline(Buffer, 80))
+		while (MapFile.getline(Buffer, 80))
 		{
 			std::string Line = Buffer;
 			for (int X = 0; X < Line.size(); ++X)
 			{
 				if (Line[X] == '*')
 				{
-					AActor* Wall = new AWall();
-					Wall->SetActorLocation(FVector2D(X, Y));
-					//Wall->SetShape(Line[X]);
-					World->SpawnActor(Wall);
+					AActor* NewActor = new AWall();
+					NewActor->SetActorLocation(FVector2D(X, Y));
+					//NewActor->SetShape(Line[X]);
+					World->SpawnActor(NewActor);
 				}
 				else if (Line[X] == 'P')
 				{
-					AActor* Player = new APlayer();
-					Player->SetActorLocation(FVector2D(X, Y));
-					//Player->SetShape(Line[X]);
-					World->SpawnActor(Player);
+					AActor* NewActor = new APlayer();
+					NewActor->SetActorLocation(FVector2D(X, Y));
+					//NewActor->SetShape(Line[X]);
+					World->SpawnActor(NewActor);
 				}
 				else if (Line[X] == 'M')
 				{
-					AActor* Monster = new AMonster();
-					Monster->SetActorLocation(FVector2D(X, Y));
-					//Monster->SetShape(Line[X]);
-					World->SpawnActor(Monster);
+					AActor* NewActor = new AMonster();
+					NewActor->SetActorLocation(FVector2D(X, Y));
+					//NewActor->SetShape(Line[X]);
+					World->SpawnActor(NewActor);
 				}
 				else if (Line[X] == 'G')
 				{
-					AActor* Goal = new AGoal();
-					Goal->SetActorLocation(FVector2D(X, Y));
-					//Goal->SetShape(Line[X]);
-					World->SpawnActor(Goal);
+					AActor* NewActor = new AGoal();
+					NewActor->SetActorLocation(FVector2D(X, Y));
+					//NewActor->SetShape(Line[X]);
+					World->SpawnActor(NewActor);
 				}
-				else if (Line[X] == ' ')
+
 				{
-					AActor* Floor = new AFloor();
-					Floor->SetActorLocation(FVector2D(X, Y));
-					//Floor->SetShape(Line[X]);
-					World->SpawnActor(Floor);
+					AActor* NewActor = new AFloor();
+					NewActor->SetActorLocation(FVector2D(X, Y));
+					//NewActor->SetShape(' ');
+					World->SpawnActor(NewActor);
 				}
 			}
 			Y++;
-
-			
 		}
-	}
-	else
-	{
-		std::cout << "맵 로드 안됨";
-	}
 
-	Map.close();
+	}
+	MapFile.close();
 
 	World->SortActor();
 
 	//UE Gameplay Framework
-	//빠져있었음
 	World->SpawnActor(new AGameMode());
 }
-
 
 void FEngine::Run()
 {
@@ -138,9 +128,6 @@ void FEngine::Run()
 	{
 		Timer->Tick();
 
-		//첫프레임은 0으로 초기화
-		/*float StartTime = SDL_GetTicks64();
-		DeltaSeconds = SDL_GetTicks64() - StartTime;*/
 		if (SDL_PollEvent(&MyEvent))
 		{
 			switch (MyEvent.type)
@@ -154,6 +141,7 @@ void FEngine::Run()
 		Input();
 		Tick();
 		Render();
+
 	}
 }
 
@@ -169,10 +157,10 @@ void FEngine::Term()
 void FEngine::Input()
 {
 	InputDevice->Tick();
-	/*if (_kbhit())
-	{
-		UInput::KeyCode = _getch();
-	}*/
+	//if (_kbhit())
+	//{
+	//	KeyCode = _getch();
+	//}
 }
 
 void FEngine::Tick()
@@ -182,7 +170,6 @@ void FEngine::Tick()
 
 void FEngine::Render()
 {
-	//system("cls");
 	SDL_SetRenderDrawColor(MyRenderer, 255, 255, 255, 0);
 	SDL_RenderClear(MyRenderer);
 
@@ -190,6 +177,7 @@ void FEngine::Render()
 
 	SDL_RenderPresent(MyRenderer);
 }
+
 
 double FEngine::GetWorldDeltaSeconds() const
 {
